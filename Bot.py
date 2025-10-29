76fe50a609af8c586cee46845b3c546310fa9508
chaudharydipti98-ops <chaudharydipti98@gmail.com>
2025-10-29 08:26:16 +0530

    Modify Telegram message sending function
    Updated Telegram API call with new token and chat ID.

--- Bot.py
+++ Bot.py
@@ -13,8 +13,8 @@ LIMIT = 200
 # ====== FUNCTIONS ======
 def send_telegram(msg):
     try:
-        requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-                      data={"chat_id": CHAT_ID, "text": msg})
+        requests.post(f"https://api.telegram.org/bot {"7955821961:AAHPVSeeSKkOOj3K6E01dYfLK7sF9Nf5DSU}"/sendMessage",
+                      data={"chat_id":6851021232, "text": entry,tp,sl})
     except Exception as e:
         print("Telegram error:", e)
 
